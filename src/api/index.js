bf906015ede498f282285d6bfe8856f7694611bd
Feedot <olehfedot@gmail.com>
2020-09-11 16:37:37 +0300

    Customized fetch for JWT_TOKEN authorization

---
+++ src/api/index.js
@@ -0,0 +1,50 @@
+import { logOut } from "./auth";
+
+const baseURLs = {
+    development: 'http://localhost:3000',
+    production: ''
+}
+
+const fetchWrapper = (url, auth = true, method = "GET", body) => {
+    const JWT_TOKEN = localStorage.getItem('JWT_TOKEN');
+    const headers = {
+        'Content-Type': 'application/json',
+        // set JWT to header if request needs auth header and user authorized
+        ...(auth && JWT_TOKEN ? { Authorization: `Bearer ${JWT_TOKEN}` } : {} )
+    };
+
+    // redirect to auth page if authorization is required
+    // and user is not authorized
+    if(auth && !JWT_TOKEN) {
+        logOut();
+    }
+
+    const options = {
+        headers,
+        method,
+        ...( body ? { body: JSON.stringify(body) } : {})
+    };
+
+    return fetch(`${baseURLs[process.env.NODE_ENV]}${url}`, { ...options })
+        .then(result => result.json())
+        .then(result => result)
+        .catch((result) => {
+            if(result.status !== 401) return result;
+
+            // logout and redirect to auth page
+            // if user get unauthorized status
+            logOut();
+        })
+
+};
+
+const getCustomFetch = () => ({
+    post: (url, body, auth) => fetchWrapper(url, auth, "POST", body),
+    get: (url) => fetchWrapper(url)
+});
+
+// using fetcher
+// fetcher.post(url, body)
+// fetcher.get(url)
+// can be customized for query params and delete, put methods.
+export const fetcher = getCustomFetch();
