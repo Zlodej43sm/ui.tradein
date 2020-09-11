6506080969b9fd7ff191c39121432b172419efde
Feedot <olehfedot@gmail.com>
2020-09-11 12:50:27 +0300

    React-router integration

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// node modules
+import React from 'react';
+
+// local components
+import Homepage from '../pages/home_page';
+
+const routes = [
+    {
+        path: '/',
+        component: props => <Homepage {...props} />,
+        exact: true,
+    }
+]
+
+export default routes;
