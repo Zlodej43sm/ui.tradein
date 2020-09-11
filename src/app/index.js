b061b13ff0d19a72fbd4778d5726d3823c8b38d5
Feedot <olehfedot@gmail.com>
2020-09-11 12:25:29 +0300

    react-i18next translator integration

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
+// node modules
 import React from 'react';
+import { useTranslation } from "react-i18next";
 
-const App = () => <div>My app</div>;
+const App = () => {
+    const { t } = useTranslation();
+    return <div>{t('common:test_value')}</div>;
+}
 
 export default App;
---
+++ src/i18next/config.js
@@ -0,0 +1,22 @@
+// node modules
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// local files
+import en from './en';
+
+// init options https://www.i18next.com/overview/configuration-options;
+i18next
+    .use(initReactI18next)
+    .init({
+        react: { wait: true },
+        resources: { en },
+        lng: 'en',
+        fallbackLng: 'en',
+        keySeparator: '.',
+        nsSeparator: ':',
+        interpolation: { escapeValue: false },
+        fallbackNS:[]
+    })
+
+export default i18next;
