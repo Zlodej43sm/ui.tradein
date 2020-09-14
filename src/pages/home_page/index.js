975b15a9f7c4106b5f02d4db8a3c87972af6cf95
Feedot <olehfedot@gmail.com>
2020-09-14 19:09:29 +0300

    fix eslint warnings

--- src/pages/home_page/index.js
+++ src/pages/home_page/index.js
@@ -1,12 +1,14 @@
 // node modules
 import React from 'react';
 
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const App = () => {
-  return <div>
-    <Link to="/auth">recover</Link>
-  </div>;
+  return (
+    <div>
+      <Link to="/auth">recover</Link>
+    </div>
+  );
 };
 
 export default App;
