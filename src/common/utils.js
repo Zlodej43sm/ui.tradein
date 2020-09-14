d5849e3a2f35e50ee3f4ac9b7c7bd1f64e1ac8df
Feedot <olehfedot@gmail.com>
2020-09-14 22:22:59 +0300

    set current language to variable

--- src/common/utils.js
+++ src/common/utils.js
@@ -3,7 +3,8 @@ import { languages } from './config';
 export const getLanguage = () => {
   const storageLanguage = localStorage.getItem('lang_key');
   const browserLanguage = navigator.language.split('-')[0];
-  const language = storageLanguage || browserLanguage;
+  const defaultLanguage = storageLanguage || browserLanguage;
+  const currentLanguage = languages[defaultLanguage];
 
-  return languages[language] ? languages[language].value : 'en';
+  return currentLanguage ? currentLanguage.value : 'en';
 };
