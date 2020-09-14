7dcca77ebd71b2ad07ac4a9c5fe45eb0586646ec
Feedot <olehfedot@gmail.com>
2020-09-14 22:24:17 +0300

    change lang name

--- src/common/utils.js
+++ src/common/utils.js
@@ -3,8 +3,8 @@ import { languages } from './config';
 export const getLanguage = () => {
   const storageLanguage = localStorage.getItem('lang_key');
   const browserLanguage = navigator.language.split('-')[0];
-  const defaultLanguage = storageLanguage || browserLanguage;
-  const currentLanguage = languages[defaultLanguage];
+  const language = storageLanguage || browserLanguage;
+  const preferableLang = languages[language];
 
-  return currentLanguage ? currentLanguage.value : 'en';
+  return preferableLang ? preferableLang.value : 'en';
 };
