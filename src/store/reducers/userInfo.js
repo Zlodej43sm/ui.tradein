c00bd262a5d3ab687d1f858496e72bc2b9755bdf
Feedot <olehfedot@gmail.com>
2020-09-15 21:31:10 +0300

    add auth functionality

---
+++ src/store/reducers/userInfo.js
@@ -0,0 +1,19 @@
+// node modules
+import { createReducer } from '@reduxjs/toolkit';
+
+// local files
+import { SET_USER_INFO, REMOVE_USER_INFO } from '../types';
+
+const initialState = {
+  email: null,
+  id: null,
+  roles: null,
+  username: null
+};
+
+export const userInfo = createReducer(initialState, {
+  [SET_USER_INFO]: (state, { payload }) => ({
+    ...payload
+  }),
+  [REMOVE_USER_INFO]: () => initialState
+});
